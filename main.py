394f7ca62308ff30d2a0cf320d2adc4f7b3c659b
Lucas Jaggernauth <lukejagg@gmail.com>
2021-11-13 18:41:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+# Tab title and icon
 st.set_page_config(page_title="Amazing", page_icon="🤓")
+
+# Document content
 st.title("Amazing")
 st.write("Hello world")
+
+# Side bar
+st.sidebar.title("Sidebar")
+st.sidebar.write("Nice, a sidebar.")
+st.sidebar.button("Click me")
